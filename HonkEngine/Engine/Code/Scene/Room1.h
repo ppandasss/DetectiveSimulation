8a003b5c3723822c8fab4343c581772503ff99e6
Kantapich Panpein <50790210+FadeZx@users.noreply.github.com>
2024-01-15 14:43:16 +0700

    Cabin pos not move
    Cabin pos move with camera pos when change scene

--- HonkEngine/Engine/Code/Scene/Room1.h
+++ HonkEngine/Engine/Code/Scene/Room1.h
@@ -13,7 +13,9 @@ public:
 	{
 	
 		//GameObject* bank = new RenderGameObject("Boss", "Assets/Images/awesomeface.png");
-		RenderGameObject* room = new RenderGameObject("cabin1", "Assets/Images/passenger_room1.png",glm::vec3(0.0f,0.0f,0.0f));
+		
+		
+		RenderGameObject* room = new RenderGameObject("cabin1", "Assets/Images/passenger_room1.png",glm::vec3(0.0f, 0.0f, 0.0f));
 		room->SetScale(glm::vec3(20.0f, 12.0f, 0.0f));
 
 		
@@ -25,6 +27,7 @@ public:
 	}
 
 	void Update(float dt, long frame) {
+
 		Scene::Update(dt, frame); // Call the base class update
 
 		if (input.Get().GetKeyDown(GLFW_KEY_E))
@@ -33,7 +36,22 @@ public:
 			std::cout << "Button pressed" << std::endl;
 			Application::Get().SetScene("Hallway");
 		}
+
+		GameObject* cabinObject = GetGameObjectByName("cabin1");
+		if (cabinObject) {
+			// Cast to Player* if necessary, or directly use if GetPosition is part of GameObject
+			RenderGameObject* cabin = dynamic_cast<RenderGameObject*>(cabinObject);
+			if (cabin) {
+
+				// Get the player's position
+				glm::vec3 camPos = glm::vec3(camera.GetPosX(),camera.GetPosY(),0.0f);
+
+				cabin->SetPosition(camPos);
+
+			}
+		}
 	}
 
 	Input& input = Application::GetInput();
+	Camera& camera = Application::GetCamera();
 };
