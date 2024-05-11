cadf72e0a5eabd2ea58c6dc3b0adf21b34dd2955
aishakwatra <92969365+aishakwatra@users.noreply.github.com>
2024-05-11 18:35:01 +0700

    set up main menu class

---
+++ HonkEngine/Engine/Code/Scene/MainMenu.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Scene.h"
+#include "../UI/UIButton.h"
+
+class MainMenu : public Scene {
+
+private:
+
+public:
+	MainMenu() {
+
+	}
+
+	void OnEnter() override {
+	}
+
+	void OnExit() override {
+		
+
+	}
+
+
+};
